8ef6e636fc8da4b871bf762fb0d883a96a04cac1
junwo <loust3598@naver.com>
2021-09-10 22:37:49 +0900

    public ipv4 address

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG") == 'True'
 
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','myblog-env.eba-c2jikfp3.ap-northeast-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','myblog-env.eba-c2jikfp3.ap-northeast-2.elasticbeanstalk.com','3.38.117.43']
 
 
 # Application definition
